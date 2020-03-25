cb7b8f37dfcb622e113bb4cb963b6475e783db52
MorvidAngel <5d7n2s9m6b@live.com>
2020-03-25 18:01:10 -0400

    learning more of HTML

---
+++ ModernJavaScript/jsFundamentals/app.js
@@ -0,0 +1,41 @@
+// Log to console
+
+/*
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.timeEnd('Hello');
+console.log('Hello World');
+*/
+
+/* 
+  multi
+  line comments
+*/
+
+// var, let, const
+var name = 'John Doe';
